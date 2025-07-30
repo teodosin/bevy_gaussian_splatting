b2f9eaca05ef2934ac45b3df9cc18d9c1210c90c
teodosin <31853349+teodosin@users.noreply.github.com>
2025-07-30 14:28:40 +0000

    Deploying to www from @ teodosin/bevy_gaussian_splatting@a8f680699f52360870c8a15172e7d293e066193a 🚀

---
+++ out/bevy_gaussian_splatting_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure8101_externref_shim: (a: number, b: number, c: any) => void;
+export const closure78954_externref_shim: (a: number, b: number, c: any) => void;
+export const closure79012_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0edb948f02f3fc1e: (a: number, b: number) => void;
+export const closure79022_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const __wbindgen_start: () => void;
